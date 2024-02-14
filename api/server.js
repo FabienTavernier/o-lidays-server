d1b68188f6152b4a65a50d05afa02d4ddd50ba60
Fabien TAVERNIER <fabien.tavernier@oclock.io>
2024-02-14 11:25:37 +0100

    feat(favorites): return rentals with favorites

--- api/server.js
+++ api/server.js
@@ -11,10 +11,10 @@ const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 
-const { users } = JSON.parse(fs.readFileSync('./db.json', 'UTF-8'));
+const db = JSON.parse(fs.readFileSync('./db.json', 'UTF-8'));
 
 function getUser({ email, password }) {
-  return users.find(
+  return db.users.find(
     (user) => user.email === email && user.password === password
   );
 }
@@ -25,10 +25,28 @@ function createToken(payload) {
   });
 }
 
+function checkIfLogged(req, res, next) {
+  if (!req.user) {
+    console.log('<< 401 UNAUTHORIZED');
+    const status = 401;
+    const message =
+      "Vous n'êtes pas autorisé à visiter cette page. Merci de vous connecter.";
+
+    res.status(status).json({ status, message });
+  } else {
+    next();
+  }
+}
+
 server.use(
   cors({
     // whitelist des origines
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'http://localhost:5174',
+      'http://127.0.0.1:5174',
+    ],
   })
 );
 
@@ -40,6 +58,23 @@ server.get('/', function (req, res) {
 });
 
 server.use('/api', middlewares);
+
+// Add user to req if token exists and is valid
+server.use((req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (authorization) {
+    const [, token] = authorization.split(' ');
+    try {
+      const jwtContent = jwt.verify(token, JWT_SECRET_KEY);
+      req.user = jwtContent;
+    } catch (err) {
+      console.log('Invalid token', err);
+    }
+  }
+
+  next();
+});
 
 // Login route
 server.post('/api/login', function (req, res) {
@@ -67,6 +102,31 @@ server.post('/api/login', function (req, res) {
   delete data.password;
   // on retourne les infos utilisateur (sans le password)
   return res.status(200).json(data);
+});
+
+// Favorites route
+server.get('/api/favorites', checkIfLogged, function (req, res) {
+  console.log('>> GET /favorites', req.user);
+
+  const user = db.users.find((u) => u.id === req.user.id);
+
+  const favorites = db.favorites.filter((f) => f.userId === user.id);
+  console.log(favorites);
+  const favoritesID = favorites.map((f) => f.rentalId);
+  console.log(favoritesID);
+  const rentals = db.rentals.filter((r) => favoritesID.includes(r.id));
+  // console.log(rentals);
+
+  const rentalsWithFavorites = rentals.map((r) => {
+    return {
+      ...r,
+      favorites: db.favorites.filter((f) => f.rentalId === r.id),
+    };
+  });
+
+  res.status(200).json({
+    favorites: rentalsWithFavorites,
+  });
 });
 
 server.use('/api', router);
